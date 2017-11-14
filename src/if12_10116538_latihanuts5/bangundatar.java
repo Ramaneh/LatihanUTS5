084f7be267e4b00d225baaabcf30f351783a187d
Asus <Asus@localhost>
2017-11-14 23:36:03 +0700

    Latihan UTS 5

---
+++ src/if12_10116538_latihanuts5/bangundatar.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if12_10116538_latihanuts5;
+
+/**
+ *
+ * @author Asus
+ */
+public interface bangundatar {
+  
+   public void hitungLuas();
+   public void hitungKeliling();
+}
+
+
